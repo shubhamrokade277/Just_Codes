b2a1afa2e2c6a8529c5e7b87ec1b0d45ae0cd79a
Shubham <shubhamrokade277@gmail.com>
2024-06-30 09:34:29 +0530

    "Deleted BinaryTrees.java file"

---
+++ Tree/BinaryTree.java
@@ -0,0 +1,42 @@
+class Node{
+    int data;
+    Node left;
+    Node right;
+
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+public class BinaryTree {
+    Node root;
+
+	public void insert(int data) {
+        root = insertRec(root, data)
+	}
+
+    public Node insertRec(Node root, int data){
+        if (root == null) {
+            root = new Node(data);
+        }
+        else if (data<root.data) {
+            root.left = insertRec(root.left, data);
+        }
+        else if (data>root.data) {
+            root.right = insertRec(root.right, data);
+        }
+        return root;
+    }
+
+    public void inorder(){
+
+    }
+
+    public void inorderRec(Node root){
+        if (root == null) {
+            inorderRec(root.left);
+            System.out.println(root.data+" ");
+            inorderRec(root.right);
+    }
+
+}
